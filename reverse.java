f6941c7879f2caabcdd70fe504a9959fcd6db3a5
Andrew Tomko <tomand285@gmail.com>
2019-05-03 08:46:41 -0500

    Created the reverse object

--- reverse.java
+++ reverse.java
@@ -2,14 +2,15 @@ public class reverse {
 
 	public static void main(String[] args) {
 		String word = "The world's best demo!";
-		String newWord = reverseWord(word);
+		reverse reverse = new reverse();
+		String newWord = reverse.reverseWord(word);
 		System.out.println(newWord);
 	}
 	
-	private static String reverseWord(String word) {
+	private String reverseWord(String word) {
 		String result = "";
 		String[] wordArray = word.split("");
-		for(int i=wordArray.length-1;i>=0;i--) {
+		for(int i=wordArray.length-1;i>-1;i--) {
 			result += wordArray[i];
 		}
 		return result;
